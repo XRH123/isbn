557a05c82d9f2782de6fc55140c4e4d34fad281c
XRH <1628636300@qq.com>
2019-04-30 17:41:52 +0800

    提交

--- isbn/src/mixins/storage.js
+++ isbn/src/mixins/storage.js
@@ -22,16 +22,16 @@ export default class StorageMixin extends wepy.mixin {
 			let count = 0
 			this[key].forEach((book) => {
 				if (book[pk] === data[pk]) {
-					count++
+					count++;
 				}
 			})
 			if (count <= 0) {
 				this[key].push(data)
 				let stringifyObj = JSON.stringify(this[key])
 				if (mode === 'sync') {
-					wx.setStorageSync(key, stringifyObj)
+					wx.setStorageSync(key, stringifyObj);
 				}	else {
-					wx.setStorage(key, stringifyObj)
+					wx.setStorage(key, stringifyObj);
 				}
 			} else {
 				return '不可重复添加'
@@ -60,5 +60,53 @@ export default class StorageMixin extends wepy.mixin {
 		} catch(e) {
 			console.log(e)
 		}
+	}
+	/*
+	** pk 数据的主键
+	** val 主键的值
+	** @return 返回主键对应的数据
+	*/
+	getDataByPK(key, pk, val) {
+		let res = {};
+		this[key].forEach((item) => {
+			if (item[pk] === val) {
+				res = item;
+			}
+		})
+		return res;
+	}
+	/*
+	** 根据主键获取缓存数据的位置
+	** param说明
+	** key 操作的数据
+	** isbn 书籍条形码值
+	** pk 数据的唯一字段
+	** val 主键的值
+	** @return 返回主键对应的数据位置
+	*/
+	getIndexByPK(key, pk, val) {
+		let index = -1;
+		this[key].forEach((item, i) => {
+			if (item[pk] === val) {
+				index = i;
+			}
+		})
+		return index;
+	}
+	/*
+	** 根据pk字段设置数据字段
+	** params说明
+	** key 操作的数据的标识
+	** pk 操作的数据的主键
+	** pkVal 操作的数据的主键值
+	** attr 要设置的属性名
+	** attrVal 要设置的属性值
+	*/
+	setAttrByPK(key, pk, pkVal, attr, attrVal) {
+		let data = this.getDataByPK(key, pk, pkVal);
+		let index = this.getIndexByPK(key, pk, pkVal);
+		data[attr] = attrVal;
+		this[key][index] = data;
+		wx.setStorageSync(key, JSON.stringify(this[key]))
 	}
 }
